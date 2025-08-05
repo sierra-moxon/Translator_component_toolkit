8f71aa7b953a70ef45770e70993265568b6c3cf5
yjzhang <yjzhang@cs.washington.edu>
2025-08-05 15:15:40 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TCT',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         # List your library's dependencies here
@@ -14,7 +14,6 @@ setup(
         'networkx',
         'numpy',
         'openai',
-        'ipycytoscape'
     ],
     entry_points={
         'console_scripts': [
