18cb86c84fa7d955ef0fd829f28f1ce3833c76e2
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2026-03-25 09:26:39 -0700

    add fix for JSON passing into query method, add tests

--- TCT/trapi.py
+++ TCT/trapi.py
@@ -12,7 +12,7 @@ import requests
 # TODO: incorporate object ids into the method.
 def build_query(subject_ids:list[str],
         object_categories:list[str], predicates:list[str],
-        return_json:bool=True,
+        return_json:bool=False,
         object_ids=None, subject_categories=None):
     """
     This constructs a query json for use with TRAPI. Queries are of the form [subject_ids]-[predicates]-[object_categories].
@@ -33,7 +33,7 @@ def build_query(subject_ids:list[str],
         A list of predicates that we are interested in. Example: ["biolink:positively_correlated_with", "biolink:physically_interacts_with"].
 
     return_json
-        If true, returns a json string; if false, returns a dict.
+        If true, returns a json string; if false, returns a dict (default).
 
     object_ids
         None by default
@@ -42,7 +42,7 @@ def build_query(subject_ids:list[str],
 
     Returns
     -------
-    A json string
+    A dict (default) or json string if return_json=True
 
     Examples
     --------
@@ -94,7 +94,7 @@ def process_result(result:dict):
     """
 
 
-def query(url:str, query:str):
+def query(url:str, query:dict):
     """
     Queries a single TRAPI endpoint.
 
@@ -102,8 +102,8 @@ def query(url:str, query:str):
     ------
     url : str
         The URL for the API endpoint.
-    query : str
-        A JSON string representing the query, as produced by build_query
+    query : dict
+        A dict representing the query, as produced by build_query
 
     Returns
     -------
